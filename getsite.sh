30cb11720e46e16078a7da14398327e2db9362b8
ryanemax <ryanemax@icloud.com>
2016-02-06 05:23:33 -0500

    init v0.8

---
+++ getsite.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ $# -eq 0 ]       ##判断参数是否存在
+then
+	echo "请输入站点地址，如：./getsite.sh http://www.baidu.com"
+	exit                     ##不存在退出
+fi
+targetSite=$1
+wget -r -p -np -k -e robots=off ${targetSite}
