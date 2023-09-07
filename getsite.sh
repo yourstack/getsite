121725a91df24aa61f29923bd060809488fd85da
ryanemax <ryanemax@gmail.com>
2023-09-07 23:41:27 +0800

    fix: readme.md & .gitignore

--- getsite.sh
+++ getsite.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 if [ $# -eq 0 ]       ##判断参数是否存在
 then
-	echo "请输入站点地址，如：./getsite.sh http://www.baidu.com"
+	echo "请输入站点地址，如：./getsite.sh http://www.xxx.com"
 	exit                     ##不存在退出
 fi
 targetSite=$1
-wget -r -p -np -k -e robots=off --restrict-file-names=unix ${targetSite}
+
+# --no-clobber 不要覆盖已存在文件
+mkdir -p site
+cd site
+wget -r -p -np -k -e robots=off --restrict-file-names=unix ${targetSite} --no-clobber
--- rsync.sh
+++ rsync.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 ## 同步所有抓取的站点模板到线上服务器
 ## http://case.anasit.com/xxx.xxx.com
-rsync -azP ./ root@api.anasit.com:/var/www/template
+rsync -azP ./ root@api.xxx.com:/var/www/demo_case
